d51352232a7341b6896b098fabce7a3ac1ba2baf
= <joshuarose099@gmail.com>
2023-02-25 16:23:01 +1100

    feat: player stubs a draw stub w/ docstring & a render stub

---
+++ src/player.py
@@ -0,0 +1,32 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2023 Joshua Rose
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the conditions attatched in the license file.
+"""
+
+import pygame
+from size import Size
+
+class Player():
+
+    name = "The Dude"
+    size = Size(width=50, height=50)
+
+    @staticmethod
+    def render_name():
+        """ Render name above player
+            returns: N/A
+        """
+        ...
+
+    def draw(self, surface):
+        """ Draw player image to surface
+            returns: N/A
+        """
