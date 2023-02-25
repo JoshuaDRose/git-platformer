2092984de0fc42e9dff06d4d154f8033ea43a2a3
= <joshuarose099@gmail.com>
2023-02-25 16:40:41 +1100

    refactor(chore): tile class w/o stub

---
+++ src/tile.py
@@ -0,0 +1,19 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2023 Joshua Rose
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the conditions attatched in the license file.
+"""
+
+import pygame
+from size import Size
+
+
+class Tile(object):
+    """ Collideable, non-moveable tile object """
