c08d40c1eaae59ce1e63dbcf732af60c3f2a9b34
= <joshuarose099@gmail.com>
2023-02-25 16:45:39 +1100

    feat: draw method note: uses player draw method (branch not merged yet)

--- src/tile.py
+++ src/tile.py
@@ -14,6 +14,15 @@ furnished to do so, subject to the conditions attatched in the license file.
 import pygame
 from size import Size
 
+from pygame.math import Vector2 as Vector
 
 class Tile(object):
     """ Collideable, non-moveable tile object """
+    def __init__(self, spawn_position: Vector):
+        self.surface = pygame.Surface(100, 100)
+        self.surface.fill((0, 0, 0, 255))
+        self.rect = pygame.Rect(100, 100, spawn_position.x, spawn_position.y)
+
+    def draw(self, surface):
+        """ Draws tile to surface at rect position """
+        surface.blit(self.surface, self.rect)
